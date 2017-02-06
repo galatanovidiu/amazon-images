4d6d9289a566c17f0d89e6f81bc278fc2b88157b
Galatan ovidiu Iulian <galatanovidiu@gmail.com>
2017-02-06 08:07:54 +0200

    Add larave Facade

---
+++ src/Facade/AmazonProductImages.php
@@ -0,0 +1,11 @@
+<?php
+
+class AmazonProductImages extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'AmazonProductImages'; }
+}
